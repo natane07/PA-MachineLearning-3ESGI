7e404c0549f47635f10760c0d38d25c135fd1bb9
Natane <natane-b@hotmail.fr>
2020-07-01 11:23:18 +0200

    Save model json mlp

--- Project/App/Server/web_server_django/ml_api/urls.py
+++ Project/App/Server/web_server_django/ml_api/urls.py
@@ -5,6 +5,6 @@ from . import views # import views so we can use them in urls.
 
 urlpatterns = [
     url(r'^$', views.index), # "/store" will call the method "index" in "views.py"
-    url(r'test', views.save_events_json),  # "/store" will call the method "index" in "views.py"
+    url(r'save_mlp', views.save_mlp_json),  # "/store" will call the method "index" in "views.py"
 
 ]
--- Project/App/Server/web_server_django/ml_api/views.py
+++ Project/App/Server/web_server_django/ml_api/views.py
@@ -3,30 +3,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.http import JsonResponse
+
 
 def index(request):
     message = "Salut tout le monde !"
     return HttpResponse(message)
 
 @csrf_exempt
-def save_events_json(request):
+def save_mlp_json(request):
     if request.method == 'POST':
-        print('Raw Data: "%s"' % request.body)
         data = request.body.decode('utf-8')
-        received_json_data = json.loads(data)
-        print(received_json_data['data'])
-        for i in received_json_data['data']:
-            print(i['titi'])
-    return HttpResponse("OK")
-
-# JSON EXEMPLE
-# {
-#     "data": [
-#         {
-#         "titi": 1
-#         },
-#         {
-#         "titi": 2
-#         }
-#     ]
-# }
+        response_data = {}
+        if data:
+            received_json_data = json.loads(data)
+            handle1 = open('mlp.json', 'w+')
+            json_str = json.dumps(received_json_data, indent=4)
+            handle1.write(json_str)
+            handle1.close()
+            response_data['success'] = True
+            response_data['message'] = 'Le model MLP est sauvegardé'
+        else:
+            response_data['success'] = False
+            response_data['message'] = 'Not body json'
+    return JsonResponse(response_data)
