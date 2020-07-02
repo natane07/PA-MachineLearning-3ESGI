a0405b979403455b6a37f6865c3e56f02f3710b0
Natane <natane-b@hotmail.fr>
2020-07-02 13:26:34 +0200

    Fix CORS_ORIGIN_ALLOW_ALL

--- Project/App/Server/web_server_django/web_server_django/settings.py
+++ Project/App/Server/web_server_django/web_server_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'corsheaders',
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
@@ -46,6 +47,7 @@ INTERNAL_IPS = ['127.0.0.1']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://192.168.1.119:8080',
+)
