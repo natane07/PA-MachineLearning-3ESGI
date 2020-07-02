fe0e2628cdf298d09b831a994e5f3be1225ac0af
Natane <natane-b@hotmail.fr>
2020-07-02 13:27:08 +0200

    Ajout de la prédiction MLP et du chargement du model

--- Project/App/Client/web-machine-learning/src/views/MachineLearning.vue
+++ Project/App/Client/web-machine-learning/src/views/MachineLearning.vue
@@ -10,23 +10,42 @@
           <v-img v-if="file != null" :src="file" aspect-ratio="2" contain max-height="350"></v-img>
         </v-col>
         <v-col cols="12">
-          <v-file-input @change="downloadFile" accept="image/*" label="File input" prepend-icon="mdi-camera"></v-file-input>
+          <v-file-input @change="downloadFile" accept="image/*" label="Image de vétement" prepend-icon="mdi-camera"></v-file-input>
+        </v-col>
+        <v-col cols="6">
+          <v-file-input @change="downloadJsonFile" label="Modéle pré-entrainé MLP" accept=".json" outlined dense></v-file-input>
         </v-col>
         <v-col cols="6">
           <v-text-field
-            label="Framework Machine Learning"
-            value="Test"
+            label="Framework Machine Learning (MLP)"
+            :value="mlpValue"
+            v-model="mlpValue"
             outlined
             readonly
-          ></v-text-field>
+          >sdcsdv</v-text-field>
+        </v-col>
+        <v-col cols="6">
+          <v-btn @click="sendJsonFile" block rounded color="primary">Charger le modéle MLP</v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-btn @click="sendImage" block rounded color="primary">Lancer la prédiction MLP</v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-file-input label="Modéle pré-entrainé Tensorflow" accept=".json" outlined dense></v-file-input>
         </v-col>
         <v-col cols="6">
           <v-text-field
             label="Framework Tensorflow"
-            value="Test"
+            value=""
             outlined
             readonly
-          ></v-text-field>
+          >toto</v-text-field>
+        </v-col>
+        <v-col cols="6">
+          <v-btn block rounded color="primary">Charger le modéle Tensorflow</v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-btn block rounded color="primary">Lancer la prédiction Tensorflow</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -35,16 +54,31 @@
 
 <script>
 // @ is an alias to /src
+import axios from 'axios'
+
 export default {
   name: 'MachineLearning',
   data () {
     return {
-      file: null
+      file: null,
+      jsonFile: '',
+      mlpValue: '',
+      event: null
     }
   },
   methods: {
+    downloadJsonFile (event) {
+      console.log(event)
+      const reader = new FileReader()
+      reader.readAsText(event, 'UTF-8')
+      reader.onload = evt => {
+        this.jsonFile = evt.target.result
+      }
+      console.log(this.jsonFile)
+    },
     downloadFile (event) {
       console.log(event)
+      this.event = event
       if (event) {
         this.createImage(event)
       } else {
@@ -57,7 +91,51 @@ export default {
       reader.onload = (e) => {
         this.file = e.target.result
       }
+      console.log(this.file)
       reader.readAsDataURL(file)
+      console.log(this.file)
+    },
+    sendImage () {
+      this.mlpValue = ''
+      if (this.event || this.jsonFile !== '') {
+        console.log(this.event)
+        const formData = new FormData()
+        formData.append('image', this.event)
+        axios.post('http://127.0.0.1:8000/ml/predict_image', formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }).then((result) => {
+          console.log(this.mlpValue)
+          console.log(result.data)
+          this.mlpValue = result.data.message
+        })
+          .catch(function () {
+            console.log('FAILURE!!')
+          })
+      } else {
+        console.log('Image or Json file not found')
+      }
+    },
+    sendJsonFile () {
+      if (this.jsonFile) {
+        console.log(this.jsonFile)
+        axios.post('http://127.0.0.1:8000/ml/save_mlp', this.jsonFile,
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((result) => {
+          console.log(this.mlpValue)
+          console.log(result.data)
+        })
+          .catch(function () {
+            console.log('FAILURE!!')
+          })
+      } else {
+        console.log('file null')
+      }
     }
   }
 }
