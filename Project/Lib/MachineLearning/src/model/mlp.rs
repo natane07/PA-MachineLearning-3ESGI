c5dbf249a9be333d05f7170cf74744b0ce0d6e7d
Natane <natane-b@hotmail.fr>
2020-07-17 15:48:52 +0200

    Ajout des commentaire lib MLP

--- Project/Lib/MachineLearning/src/model/mlp.rs
+++ Project/Lib/MachineLearning/src/model/mlp.rs
@@ -10,6 +10,7 @@ use std::ffi::CStr;
 use std::str::from_utf8;
 use std::io::Read;
 
+// Structure pour la Serialiation et désérialisation
 #[derive(Serialize, Deserialize)]
 pub struct MLP_serialize {
     npl: Vec<i64>,
@@ -20,18 +21,21 @@ pub struct MLP_serialize {
 }
 
 pub struct MLP {
-    npl: Vec<i64>,
-    l: usize,
-    w: Vec<Vec<Vec<f64>>>,
+    npl: Vec<i64>, // neurone perd layers (réseau de neurone)
+    l: usize, // indice de la derniere couche du reseau
+    w: Vec<Vec<Vec<f64>>>, // poids
     deltas: Vec<Vec<f64>>,
     x: Vec<Vec<f64>>,
 }
 
 
 impl MLP {
+    // Création et initialisiation du mlp
     pub fn create(mut self, npl: Vec<i64>) -> MLP{
         let mut rand = rand::thread_rng();
+        // Couche 0
         self.w.push(vec![]);
+        // Pour chaque couche, on stock les poids
         for layer in 1..(self.l + 1) {
             self.w.push(vec![]);
             for i in 0..(npl[layer - 1] + 1) as usize{
@@ -43,6 +47,7 @@ impl MLP {
         }
 
         self.deltas.push(vec![]);
+        // Pour chaque couche, on stock les deltas (autant que le nombre de neurone par couche + biais)
         for layer in 1..(self.l + 1) {
             self.deltas.push(vec![]);
             for _j in 0..(npl[layer] + 1) as usize{
@@ -50,10 +55,12 @@ impl MLP {
             }
         }
 
+        // Pour chaque couche, on stock les x (autant que le nombre de neurone par couche + biais)
         for layer in 0..(self.l + 1) {
             self.x.push(vec![]);
             for j in 0..(npl[layer] + 1) as usize {
                 if j == 0 {
+                    // biais
                     self.x[layer].push(1.0);
 
                 } else {
@@ -65,6 +72,7 @@ impl MLP {
     }
 }
 
+// Création et initialisiation du mlp
 pub fn _create_mlp(npl: Vec<i64>, npl_size: usize) -> MLP {
     let mut mlp = MLP {
         npl: npl.clone(),
@@ -77,6 +85,7 @@ pub fn _create_mlp(npl: Vec<i64>, npl_size: usize) -> MLP {
     return mlp;
 }
 
+// Création et initialisiation du mlp (coté C)
 #[no_mangle]
 pub fn create_mlp(npl: *mut i64, npl_size: usize) -> *const MLP {
 
@@ -92,6 +101,7 @@ pub fn create_mlp(npl: *mut i64, npl_size: usize) -> *const MLP {
     println!("mlp.x:{:?}", mlp.x);*/
 }
 
+// Fonction pour sérialiser notre stucture MLP (coté C)
 #[no_mangle]
 pub fn serialized_mlp(model_ptr: *const MLP) {
     let mlp_python;
@@ -112,6 +122,7 @@ pub fn serialized_mlp(model_ptr: *const MLP) {
     serde_json::to_writer(file, &j);
 }
 
+// Fonction pour désérialiser notre stucture MLP (coté C)
 #[no_mangle]
 pub fn deserialized_mlp() -> *const MLP_serialize{
     let contents = read_to_string("mlp.json").expect("Unable to open");
@@ -120,20 +131,27 @@ pub fn deserialized_mlp() -> *const MLP_serialize{
     Box::leak(Box::new(deserialized))
 }
 
+// Prediction
 pub fn _mlp_predict_common(mlp: &mut MLP, sample_imput: Vec<f64>, classification_mode: bool) -> Vec<f64> {
+    // Copie des input dans la couche d'entre du reseau
     for j in 1..(mlp.npl[0] + 1) as usize{
         mlp.x[0][j] = sample_imput[j - 1];
     }
 
+    // Application de la formule pour la prediction
     for layer in 1..(mlp.l + 1) {
+        // Iteration sur chaque neurone
         for j in 1..(mlp.npl[layer] + 1) as usize {
+            // Somme pondérée des poids (w) * x
             let mut result: f64 = 0.0;
             for i in 0..(mlp.npl[layer - 1] + 1) as usize {
                 result += mlp.w[layer][i][j] * mlp.x[layer - 1][i];
             }
+            // Tangente hyperbolique (tanh) pour de la classification
             if layer != mlp.l || classification_mode {
                 result = result.tanh();
             }
+            // Stocké dans x(j,l)
             mlp.x[layer][j] = result;
         }
     }
@@ -141,10 +159,12 @@ pub fn _mlp_predict_common(mlp: &mut MLP, sample_imput: Vec<f64>, classification
     return mlp.x[mlp.l].clone();
 }
 
+// Prédiction pour la regression
 pub extern fn _mlp_regression(mlp: &mut MLP, sample_imput: Vec<f64>) -> Vec<f64> {
     return _mlp_predict_common(mlp, sample_imput, false);
 }
 
+// Prédiction pour la regression (cote C)
 #[no_mangle]
 pub extern fn mlp_regression(mlp: *mut MLP, sample_imput: *mut f64, sample_imput_size: usize) -> *const f64 {
     let sample_imput = unsafe { from_raw_parts(sample_imput, sample_imput_size) };
@@ -154,6 +174,7 @@ pub extern fn mlp_regression(mlp: *mut MLP, sample_imput: *mut f64, sample_imput
     return _mlp_regression(mlp, sample_imput.to_vec()).as_ptr();
 }
 
+// Récupérer la valeur max de notre prediction pour la régression (coté C)
 #[no_mangle]
 pub extern fn mlp_regression_max_value(mlp: *mut MLP, sample_imput: *mut f64, sample_imput_size: usize) -> f64 {
     let sample_imput = unsafe { from_raw_parts(sample_imput, sample_imput_size) };
@@ -170,10 +191,12 @@ pub extern fn mlp_regression_max_value(mlp: *mut MLP, sample_imput: *mut f64, sa
     return max_value;
 }
 
+// Prédiction pour la classification
 pub extern fn _mlp_classification(mlp: &mut MLP, sample_imput: Vec<f64>) -> Vec<f64> {
     return _mlp_predict_common(mlp, sample_imput, true);
 }
 
+// Prédiction pour la classification (coté C)
 #[no_mangle]
 pub extern fn mlp_classification(mlp: *mut MLP, sample_imput: *mut f64, sample_imput_size: usize) -> *const f64 {
     let sample_imput = unsafe { from_raw_parts(sample_imput, sample_imput_size) };
@@ -183,6 +206,7 @@ pub extern fn mlp_classification(mlp: *mut MLP, sample_imput: *mut f64, sample_i
     return _mlp_classification(mlp, sample_imput.to_vec()).as_ptr();
 }
 
+// Récupérer le arg_max de notre prediction (coté C)
 #[no_mangle]
 pub extern fn mlp_classification_image(mlp: *mut MLP, sample_imput: *mut f64, sample_imput_size: usize) -> i64 {
     let sample_imput = unsafe { from_raw_parts(sample_imput, sample_imput_size) };
@@ -199,6 +223,7 @@ pub extern fn mlp_classification_image(mlp: *mut MLP, sample_imput: *mut f64, sa
     return max_index;
 }
 
+// Récupérer la valeur max de notre prediction pour la classification (coté C)
 #[no_mangle]
 pub extern fn mlp_classification_max_value(mlp: *mut MLP, sample_imput: *mut f64, sample_imput_size: usize) -> f64 {
     let sample_imput = unsafe { from_raw_parts(sample_imput, sample_imput_size) };
@@ -215,25 +240,34 @@ pub extern fn mlp_classification_max_value(mlp: *mut MLP, sample_imput: *mut f64
     return max_value;
 }
 
+// Entrainement avec stochastic gradient descent
 pub extern fn mlp_train_common(mlp: &mut MLP,
                                inputs: Vec<Vec<f64>>,
                                expected_outputs: Vec<Vec<f64>>,
                                iterations: usize,
-                               alpha: f64,
+                               alpha: f64, // learning rate
                                classification_mode: bool) {
     let mut rand = rand::thread_rng();
+    // Boucle selon le nombre d'iteration
     for iterator in 0..iterations {
+        // On prend un exemple au hasard
         let mut k: usize = rand.gen_range(0, inputs.len()) as usize;
+        // Calcul des sorties de la derniere couche en propagent via le predict
         _mlp_predict_common(mlp, inputs[k].clone(), classification_mode);
+        // Pour chaque neurone de la derniere couche (sauf biais)
         for j in 1..(mlp.npl[mlp.l] + 1) as usize{
+            // Calcul des deltas pour chaque neuronne de la derniere couche
             mlp.deltas[mlp.l][j] = mlp.x[mlp.l][j] - expected_outputs[k][j - 1];
             if classification_mode {
                 mlp.deltas[mlp.l][j] *= (1f64 - (mlp.x[mlp.l][j] * mlp.x[mlp.l][j]));
             }
         }
+
+        // Calcul des deltas des couches precedentes
         for layer in -(mlp.l as i64)..-1 {
             let layer = -layer as usize;
             for i in 1..(mlp.npl[layer - 1] + 1) as usize {
+                // Calcul de la somme pondérée
                 let mut result: f64 = 0.0;
                 for j in 1..(mlp.npl[layer] + 1) as usize{
                     result += mlp.w[layer][i][j] * mlp.deltas[layer][j];
@@ -243,6 +277,7 @@ pub extern fn mlp_train_common(mlp: &mut MLP,
             }
         }
 
+        // Mise à jour des w
         for layer in 1..(mlp.l + 1) {
             for i in 0..(mlp.npl[layer - 1] + 1) as usize{
                 for j in 1..(mlp.npl[layer] + 1) as usize{
@@ -253,6 +288,7 @@ pub extern fn mlp_train_common(mlp: &mut MLP,
     }
 }
 
+// Entrainement pour la classification
 pub extern fn _mlp_train_classification(mlp: &mut MLP,
                                         inputs: Vec<Vec<f64>>,
                                         expected_outputs: Vec<Vec<f64>>,
@@ -261,6 +297,7 @@ pub extern fn _mlp_train_classification(mlp: &mut MLP,
     mlp_train_common(mlp, inputs, expected_outputs, iterations, alpha,true);
 }
 
+// Entrainement pour la classification (pour le C)
 #[no_mangle]
 pub extern fn mlp_train_classification(mlp: *mut MLP,
                                        nb_row: usize,
@@ -293,6 +330,7 @@ pub extern fn mlp_train_classification(mlp: *mut MLP,
     println!("mlp_train_classification FIN");
 }
 
+// Entrainement pour la regression
 pub extern fn _mlp_train_regression(mlp: &mut MLP,
                                     inputs: Vec<Vec<f64>>,
                                     expected_outputs: Vec<Vec<f64>>,
@@ -301,6 +339,7 @@ pub extern fn _mlp_train_regression(mlp: &mut MLP,
     mlp_train_common(mlp, inputs, expected_outputs, iterations, alpha,false);
 }
 
+// Entrainement pour la regression (pour le C)
 #[no_mangle]
 pub extern fn mlp_train_regression(mlp: *mut MLP,
                                        nb_row: usize,
