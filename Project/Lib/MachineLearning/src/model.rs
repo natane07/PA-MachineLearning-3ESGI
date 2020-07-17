d44435146a621882affe3dc49b47cbec2454f0d9
Natane <natane-b@hotmail.fr>
2020-07-17 13:53:57 +0200

    Correction et commentaire SVM

--- Project/Lib/MachineLearning/src/model.rs
+++ Project/Lib/MachineLearning/src/model.rs
@@ -1,3 +1,3 @@
 pub mod linear_model;
 pub mod mlp;
-pub mod svn;
+pub mod svm;
--- Project/Lib/MachineLearning/src/model/svm.rs
+++ Project/Lib/MachineLearning/src/model/svm.rs
@@ -1,71 +1,83 @@
 use osqp::{CscMatrix, Problem, Status, Settings};
 
-pub struct SVN {
-    p: Vec<Vec<f64>>,
-    q: Vec<f64>,
+pub struct SVM {
+    p: Vec<Vec<f64>>, // Matrice ligne/colonne
+    q: Vec<f64>, // Autant d'element que d'exemple
     l: Vec<f64>,
     u: Vec<f64>,
     a: Vec<Vec<f64>>
 }
 
-pub fn _create_svn(x_size: usize) -> SVN {
-    let mut svn = SVN {
-        p: Vec::with_capacity(x_size),
+pub fn _create_svm(x_size: usize) -> SVM {
+    let mut svm = SVM {
+        p: Vec::with_capacity(x_size), // Remplire avec autant d'exemple
         q: Vec::with_capacity(x_size),
         l: Vec::with_capacity(x_size + 1),
         u: Vec::with_capacity(x_size + 1),
         a: Vec::with_capacity(x_size + 1)
     };
-    return svn;
+    return svm;
 }
 
-fn _predict_svn(x: &[f64], w: &[f64]) -> f64 {
+fn _predict_svm(x: &[f64], w: &[f64]) -> f64 {
     let mut sum: f64 = w[0];
     for i in 0..x.len() {
         sum += x[i] * w[i + 1];
     }
     return sum.signum();
 }
-fn _train_svn(svn: &mut SVN, x_train: Vec<Vec<f64>>, y_train: Vec<f64>) -> Vec<f64>{
+fn _train_svm(svm: &mut SVM, x_train: Vec<Vec<f64>>, y_train: Vec<f64>) -> Vec<f64>{
 
     for i in 0.. x_train.len() {
-        svn.p.push(Vec::with_capacity(x_train.len()));
-        svn.q.push(-1.0);
-        if i == 0 {
-            svn.a.push(vec!());
-            svn.l.push(0.0);
-            svn.u.push(0.0);
-        }
-        svn.a.push(vec!());
-        svn.l.push(0.0);
-        svn.u.push(f64::INFINITY);
+        // Remplie la matrice, autant d'exemple que de colonne
+        svm.p.push(Vec::with_capacity(x_train.len()));
+        svm.q.push(-1.0);
 
+        if i == 0 {
+            // 0 <= alpha <= 0
+            svm.a.push(vec!());
+            svm.l.push(0.0);
+            svm.u.push(0.0);
+        }
+        // 0 <= alpha <= +Infinie
+        svm.a.push(vec!());
+        svm.l.push(0.0);
+        svm.u.push(f64::INFINITY);
+
+        // Pour chaque exemple
         for j in 0..x_train.len() {
             if i == 0 {
-                svn.a[0].push(y_train[j]);
+                svm.a[0].push(y_train[j]);
             }
 
-            svn.a[i + 1].push(if i == j { 1.0 } else { 0.0 });
+            svm.a[i + 1].push(if i == j { 1.0 } else { 0.0 });
             let mut sum = 0.0;
+            // Produit scalaire des vecteurs Xi * Transposer(Xj)
             for k in 0..x_train[i].len() {
                 sum += x_train[i][k] * x_train[j][k]
             }
-            svn.p[i].push(y_train[i] * y_train[j] * sum);
+            // Ajout des elements dans la colonne
+            svm.p[i].push(y_train[i] * y_train[j] * sum);
         }
     }
 
-    let p: Vec<&[f64]> = (svn.p.iter().map(|v | v.as_slice()).collect());
-    let q: &[f64] = svn.q.as_slice();
-    let l: &[f64] = svn.l.as_slice();
-    let u: &[f64] = svn.u.as_slice();
-    let a: Vec<&[f64]> = (svn.a.iter().map(|v | v.as_slice()).collect());
+    // Conversion en slice
+    let p: Vec<&[f64]> = (svm.p.iter().map(|v | v.as_slice()).collect());
+    let q: &[f64] = svm.q.as_slice();
+    let l: &[f64] = svm.l.as_slice();
+    let u: &[f64] = svm.u.as_slice();
+    let a: Vec<&[f64]> = (svm.a.iter().map(|v | v.as_slice()).collect());
 
+    // Création de la matrice
     let P: CscMatrix = CscMatrix::from(p).into_upper_tri();
 
+    // Parametrage des settings pour le solver
     let settings = Settings::default().verbose(false);
 
+    // Creation du probleme quadratique
     let mut prob: Problem = Problem::new(P, q, a, l, u, &settings).expect("Failed to setup");
 
+    // Resolution du probleme quadratique (récupere les alphas)
     let result: Status = prob.solve();
 
     println!("{:?}", result.x().expect("Failed to solve problem"));
@@ -93,7 +105,7 @@ fn _train_svn(svn: &mut SVN, x_train: Vec<Vec<f64>>, y_train: Vec<f64>) -> Vec<f
         }
     }
 
-    // Calcul du produit scaliare
+    // Calcul du produit scalaire
     let mut sum = 0.0;
     for i in 0..x_train[arg_max].len() {
         sum += w[i] * x_train[arg_max][i];
